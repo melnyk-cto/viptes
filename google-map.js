4cbfc392cdb3c51f5fce6bc70e69698daf082bdc
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2022-09-24 08:26:43 +0300

    fix: create google-map.js and index.js

---
+++ google-map.js
@@ -0,0 +1,15 @@
+<!--Footer Code-->
+
+<!-- begin: google maps -->
+function initialize() {
+  const fromInput = $('[name="from-to"]');
+  const durationInput = $('[name="from-duration"]');
+  const toInput = $('[name="to"]');
+
+  if (fromInput[0]) new google.maps.places.Autocomplete(fromInput[0]);
+  if (toInput[0]) new google.maps.places.Autocomplete(toInput[0]);
+  if (durationInput[0]) new google.maps.places.Autocomplete(durationInput[0]);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+<!-- end: google maps -->
